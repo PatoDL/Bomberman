1acef95f3a7b4cd142b0236418e2a6b244ef508e
Pato <patodl2701@gmail.com>
2019-05-15 22:18:09 -0300

    added player movement

---
+++ Bomberman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject wall;
+    Vector3 zMovement;
+    Vector3 xMovement;
+    void Start()
+    {
+        zMovement = new Vector3(0, 0, 1);
+        xMovement = new Vector3(1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += zMovement * wall.transform.localScale.x;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += zMovement * -wall.transform.localScale.x;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += xMovement * -wall.transform.localScale.x;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += xMovement * wall.transform.localScale.x;
+        }
+    }
+}
