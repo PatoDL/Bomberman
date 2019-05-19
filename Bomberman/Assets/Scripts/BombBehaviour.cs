cb696549034fb88f3894f7ef9f4e7ff90ab8bc62
Pato <patodl2701@gmail.com>
2019-05-19 12:44:42 -0300

    bombs explosion added

--- Bomberman/Assets/Scripts/BombBehaviour.cs
+++ Bomberman/Assets/Scripts/BombBehaviour.cs
@@ -7,6 +7,8 @@ public class BombBehaviour : MonoBehaviour
     float timer;
     bool activeTimer;
     static bool instanciated;
+    public GameObject explosionPF;
+    const int cantExplosions=4;
 
     void Start()
     {
@@ -23,6 +25,7 @@ public class BombBehaviour : MonoBehaviour
             if (timer > 2.5f)
             {
                 instanciated = false;
+                Explode();
                 Destroy(this.gameObject);
             }
         }
@@ -43,5 +46,17 @@ public class BombBehaviour : MonoBehaviour
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().isKinematic = true;
         }
+    }
+
+    void Explode()
+    {
+        GameObject e1 = Instantiate(explosionPF);
+        e1.transform.position = new Vector3(transform.position.x - explosionPF.transform.localScale.x, transform.position.y, transform.position.z);
+        GameObject e2 = Instantiate(explosionPF);
+        e2.transform.position = new Vector3(transform.position.x + explosionPF.transform.localScale.x, transform.position.y, transform.position.z);
+        GameObject e3 = Instantiate(explosionPF);
+        e3.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - explosionPF.transform.localScale.z);
+        GameObject e4 = Instantiate(explosionPF);
+        e4.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + explosionPF.transform.localScale.z);
     }
 }
---
+++ Bomberman/Assets/Scripts/ExplosionBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name=="DestroyableWall")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
